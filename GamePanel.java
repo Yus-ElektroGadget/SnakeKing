a4f43e4d368be3db1874c1ac42dd8366ae56b586
Yus-ElektroGadget <72982873+Yus-ElektroGadget@users.noreply.github.com>
2022-11-27 16:48:31 +0700

    Create GamePanel.java
    membuat game panel

---
+++ GamePanel.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+//Mekanisme ular
+
+public class GamePanel extends JPanel implements ActionListener{
+
+	static final int SCREEN_WIDTH = 1300; //set width frame
+	static final int SCREEN_HEIGHT = 750; //set height frame
+	static final int UNIT_SIZE = 50; //set ukuran unit game
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE); //set unit game
+	int DELAY = 175;
+	final int x[] = new int[GAME_UNITS]; //deklarasi array sesuai dengan ukuran game unit
+	final int y[] = new int[GAME_UNITS];
+
+	int bodyParts = 5; //set badan ular menjadi 5 blok
