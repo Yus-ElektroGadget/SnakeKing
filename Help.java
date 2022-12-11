d55ef8a2ec2d7bee3fd33adb52b99a52d3db4102
norickeriyadi <76475671+norickeriyadi@users.noreply.github.com>
2022-12-12 01:42:20 +0700

    Create Help.java
    Membuat Help.java

---
+++ Help.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.event.*;
+import java.awt.Font;
+import javax.swing.*;
+
+//Menu Help
+public class Help implements ActionListener {
+
+    //Deklarasi 
+    JLabel arrows, daftarMakanan;
+    JFrame frame = new JFrame();
+    JButton Exit = new JButton("Comeback");
