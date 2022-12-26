7b9ebb0416d8f13ac2fa608f3d5d4d338bf93115
Xancave <107187903+Xancave@users.noreply.github.com>
2022-12-26 14:55:19 +0700

    Update Sound.java

--- Sound.java
+++ Sound.java
@@ -1,4 +1,43 @@
+import java.applet.*;
+import javax.swing.*;
+import java.net.*;
 
-public class Sound {
+
+//Class Sound
+public class Sound extends JFrame{
+
+    //deklarasi
+    private AudioClip audio;
+    public boolean isPlaying;
+
+    public Sound(String SoundURL) {
+        try{
+            URL url = new URL(SoundURL); //deklarasi URL
+            audio = Applet.newAudioClip(url); //deklarasi Applet
+        }
+        catch (MalformedURLException ex){ //Get message error
+            Since:
+            System.err.println("Terdapat masalah audio : " + ex.getMessage());
+            System.exit(0); //program exit
+        }
+    }
+
+    public void play(){ //fungsi sound play
+        audio.play();
+        isPlaying = true;
+    }
+
+    public void stop(){ //fungsi sound stop
+        audio.stop();
+        isPlaying = false;
+    }
+
+    public void loop(){ //fungsi sound loop
+        audio.loop();
+    }
+
+    public boolean getIsPlaying(){ //fungsi boolean apakah masih berjalan suaranya
+        return isPlaying;
+    }
 
 }
