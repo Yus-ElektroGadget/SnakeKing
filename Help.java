bd5e4d39232b80f82a1420b8c8c78e5d4feebd74
norickeriyadi <76475671+norickeriyadi@users.noreply.github.com>
2022-12-26 11:25:54 +0700

    Update Help.java
    Menambahkan frame untuk "Help Menu"

--- Help.java
+++ Help.java
@@ -10,3 +10,19 @@ public class Help implements ActionListener {
     JLabel arrows, daftarMakanan;
     JFrame frame = new JFrame();
     JButton Exit = new JButton("Comeback");
+    
+
+    Help() {
+
+        //Buat Frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBackground(Color.BLACK);
+        frame.setSize(1300, 750);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setTitle("Help Menu");
+        frame.getContentPane().setBackground(new Color(0x123456)); // change color of background
+        ImageIcon image = new ImageIcon("image/snake.png"); // create an ImageIcon
+        frame.setIconImage(image.getImage()); // change icon of frame
+        
